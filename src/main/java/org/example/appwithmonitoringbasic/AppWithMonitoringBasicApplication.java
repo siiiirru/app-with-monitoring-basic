119dc0946ab337bf03a7c58203295bd4207e3443
siiiirru <eqeee@naver.com>
2025-06-30 16:03:29 +0900

    [feat]springboot init

---
+++ src/main/java/org/example/appwithmonitoringbasic/AppWithMonitoringBasicApplication.java
@@ -0,0 +1,13 @@
+package org.example.appwithmonitoringbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWithMonitoringBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppWithMonitoringBasicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/appwithmonitoringbasic/AppWithMonitoringBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.appwithmonitoringbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWithMonitoringBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
